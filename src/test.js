a90763b688d61f3da6cf1c0fb04f53244387117b
Anders Sjöberg <shoobah@gmil.com>
2016-01-23 00:57:03 +0100

    JSDoc added

--- src/test.js
+++ src/test.js
@@ -1,3 +1,6 @@
-var add = function (a, b) { return a + b; };
-var result = add(1, 2);
+/**
+ * Testar JSDoc
+ */
+var add = function (prefix, b) { return prefix + b; };
+var result = add('enna: ', 2);
 console.log(result);
--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,6 @@
-const add = (a: number, b: number): number => a + b;
-
-const result: number = add(1,2)
-
-console.log(result);
+/**
+ * Testar JSDoc
+ */
+const add = (prefix: string, b: number): string => prefix + b;
+const result: string = add('enna: ', 2)
+console.log(result)
